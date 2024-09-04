c0d2e14b2848147e99e34e781e1fa984568b1187
shivam-kumar9140 <155888051+shivam-kumar9140@users.noreply.github.com>
2024-09-04 16:48:47 +0000

    DATA TYPE

--- javascript/basics_1.js
+++ javascript/basics_1.js
@@ -1,1 +1,32 @@
 console.log("shivam")
+//constant andddd variables-----------------------------
+const accountId=144343
+let accountEmail="shivam@gmail.com"
+var accountPassword="234525"
+accountCity="ahamdabad"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+//what happen if we change the const value
+//accountId=365365   ------ it will show error as we assign to const
+//console.log(accountId)
+
+let accountState;
+console.log(accountState);
+//alert(3+3) // not work as it is nodejs not brower which whave inbuilt function
+
+
+// data types ------------------------
+
+let nam="shiva"
+let age=18
+let isLoggedIn=false
+let id=null
+let st=undefined
+console.log([nam,age,isLoggedIn]);
+console.log(typeof nam);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof id);
+console.log(typeof st);
+
+
---
+++ javascript/data_type_conversion.js
@@ -0,0 +1,5 @@
+//conversion in numbers----------------------------
+//example__1__------------------------
+let score="33"
+let value=Number(score)
+console.log(value);
