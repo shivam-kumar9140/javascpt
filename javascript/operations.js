e8c2c3e0472846b4221127978b7b7a834b1f2163
shivam-kumar9140 <155888051+shivam-kumar9140@users.noreply.github.com>
2024-09-05 08:46:45 +0000

    operations study

---
+++ javascript/operations.js
@@ -0,0 +1,34 @@
+let value=3
+let negValue=-value
+console.log(negValue)
+
+// basics operations----
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2)
+console.log(2%2)
+// basic string operations---
+let str="shivam"
+let str2="sir"
+let str3=str+str2
+console.log(str3)
+
+//example2-----------
+console.log(("1")+2)
+console.log(1+("2"))
+console.log(("1"+ "2"))
+console.log(("1"+2+2))
+console.log((1+2+"2"))
+//use parenthesis for operations of each expresions-------------------
+
+//example3-----------
+console.log(+true)
+console.log(+"")
+
+//example4-----------
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
+//learn more about increament and decrement-----------------
