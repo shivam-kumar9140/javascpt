3708f2da034f7beb0ed9f8042716544332234fd3
shivam-kumar9140 <155888051+shivam-kumar9140@users.noreply.github.com>
2024-09-05 08:27:50 +0000

    conversion is done

--- javascript/data_type_conversion.js
+++ javascript/data_type_conversion.js
@@ -3,3 +3,38 @@
 let score="33"
 let value=Number(score)
 console.log(value);
+
+//example2----------------------
+let score1="33abc"
+let value1=Number(score1)
+console.log(value1)
+//example3--------
+let score3=null
+let value3=Number(score3)
+console.log(value3);
+//example4---------------
+let score4=true
+let value4=Number(score4)
+console.log(value4)
+//example for string comparison---------------------
+let value5=33
+let score5=String(value5)
+console.log(score5)
+
+//example for boolean--------------
+let value6=1
+let score6=Boolean(value6)
+console.log(score6)
+
+//example for boolean--------------
+let value7=0
+let score7=Boolean(value7)
+console.log(score7)
+
+//example for boolean--------------
+let value8=""
+let score8=Boolean(value8)
+console.log(score8)
+
+
+
